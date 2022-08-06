717c1062fd0eefbe8f3624be1fe69f3c6b8cb235
OPReturnCode <mask.em@protonmail.com>
2022-08-06 21:25:54 +0100

    Add BigMap, fixes hitting NodeJS's Map size limit

--- utxos.ts
+++ utxos.ts
@@ -1,13 +1,84 @@
 type Txo = string;
 type TokenId = Buffer;
 
-class GlobalUtxoSet extends Map<Txo, TokenId> {
-    public static Instance() {
-        return this._instance || (this._instance = new GlobalUtxoSet());
+class BigMap {    
+    maps: Array<Map<Txo, TokenId>>
+    
+    constructor(...parameters: any[]) {
+        this.maps = []
+        this.pushNewMap()
     }
-    private static _instance: GlobalUtxoSet;
+    
+    private pushNewMap() {
+        this.maps.push(new Map<Txo, TokenId>())
+    }
 
-    public set(key: string, value: Buffer): this {
+    public set(key: string, value: Buffer): Map<Txo, TokenId> {
+       for (let i=0; i < this.maps.length; i++){
+            if (this.maps[i].has(key)) {
+                return this.maps[i].set(key, value)
+            }
+        }
+        
+        let lastMap = this.maps[this.maps.length - 1]
+        if (lastMap.size >= Math.pow(2, 20)){
+            this.pushNewMap()
+        }
+        lastMap = this.maps[this.maps.length - 1]
+        return lastMap.set(key, value)
+    }
+    
+    public get(key: string) {
+        for (let i=0; i < this.maps.length; i++) {
+            let value = this.maps[i].get(key)
+            if (value !== undefined) {
+                return value
+            }
+        }
+    }
+    
+    public has(key: string) {
+        for (let i=0; i < this.maps.length; i++){
+            let value = this.maps[i].get(key)
+            if (value !== undefined) {
+                return true
+            }
+        }
+        return false
+    }
+    
+    public delete(key: string) {
+        for (let i=0; i < this.maps.length; i++){
+            if (this.maps[i].has(key)) {
+                return this.maps[i].delete(key)
+            }
+        }
+        return false
+    }
+    
+    get size() {
+        let size = 0
+        for (let i=0; i < this.maps.length; i++){
+            size += this.maps[i].size
+        }
+        return size
+    }
+    public clear() {}
+    public entries() {}
+    public forEach() {}
+    public [Symbol.iterator]() {}
+    public keys() {}
+    public values() {}
+    public [Symbol.toStringTag]() {}
+}
+
+class GlobalUtxoSet<T, Y> extends BigMap {
+    public static Instance() {
+        return this._instance || (this._instance = new GlobalUtxoSet<Txo, TokenId>());
+    }
+    private static _instance: GlobalUtxoSet<Txo, TokenId>;
+
+    public set(key: string, value: Buffer): any {
         if (this.size % 100000 === 0) {
             console.log(`UTXO size: ${this.size}`);
         }
